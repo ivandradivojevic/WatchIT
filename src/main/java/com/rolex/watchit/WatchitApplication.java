c6e044b6b6ca903dbc3ec0c83eab270b4c584158
ivandradivojevic <ivandradivojevic@gmail.com>
2023-04-20 11:19:20 +0200

    Added springintializr

---
+++ src/main/java/com/rolex/watchit/WatchitApplication.java
@@ -0,0 +1,13 @@
+package com.rolex.watchit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rolex/watchit/WatchitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rolex.watchit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
